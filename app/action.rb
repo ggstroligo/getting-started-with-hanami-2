8f25aad47229e6ef290c64d19ec318acc203bff4
Augusto Stroligo <gutostroligo@gmail.com>
2022-11-28 15:31:11 -0300

    Handling 404 on base action

--- app/action.rb
+++ app/action.rb
@@ -5,5 +5,14 @@ require "hanami/action"
 
 module Bookshelf
   class Action < Hanami::Action
+    config.handle_exception ROM::TupleCountMismatchError => :handle_not_found
+
+    private
+
+    def handle_not_found(_req, response, _exception)
+      response.status = 404
+      response.format = :json
+      response.body = { error: "not_found" }.to_json
+    end
   end
 end
--- app/actions/books/show.rb
+++ app/actions/books/show.rb
@@ -6,8 +6,6 @@ module Bookshelf
       class Show < Bookshelf::Action
         include Deps["persistence.rom"]
 
-        config.handle_exception ROM::TupleCountMismatchError => :handle_not_found
-
         params do
           required(:id).value(:integer)
         end
@@ -19,14 +17,6 @@ module Bookshelf
 
           response.format = :json
           response.body = book.to_json
-        end
-
-        private
-
-        def handle_not_found(_request, response, _exception)
-          response.status = 404
-          response.format = :json
-          response.body = {error: "not_found"}.to_json
         end
       end
     end
