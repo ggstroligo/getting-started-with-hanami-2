f704d5af96e4f954d7998f93b190b3f5a97dfba1
Augusto Stroligo <gutostroligo@gmail.com>
2022-11-28 00:56:53 -0300

    Added pagination and params validations to controller

--- spec/requests/books/index/pagination_spec.rb
+++ spec/requests/books/index/pagination_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe "GET /books pagination", type: [:request, :database] do
 
   before do
     10.times do |n|
-      books.insert(title: "Title #{n}", author: "Author #{n}")
+      books.insert(title: "Book #{n}", author: "Author #{n}")
     end
   end
 
@@ -19,6 +19,23 @@ RSpec.describe "GET /books pagination", type: [:request, :database] do
         { "title" => "Book 1", "author" => "Author 1" },
         { "title" => "Book 2", "author" => "Author 2" }
       ])
+    end
+  end
+
+  context "given invalid page and per_page params" do
+    it "returns a 422 unprocessable response" do
+      get "/books?page=-1&per_page=3000"
+
+      expect(last_response).to be_unprocessable
+
+      response_body = JSON.parse(last_response.body)
+
+      expect(response_body).to eq(
+        "errors" => {
+          "page" => ["must be greater than 0"],
+          "per_page" => ["must be less than or equal to 30"]
+        }
+      )
     end
   end
 end
